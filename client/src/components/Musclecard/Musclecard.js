b4222b9c2f5391f69a7f7e921238fea4d957cdbb
Corvus <wyatta.hancock@gmail.com>
2020-10-21 12:54:06 -0600

    editing

--- client/src/components/Musclecard/Musclecard.js
+++ client/src/components/Musclecard/Musclecard.js
@@ -1,7 +1,8 @@
 import React from "react";
 import data from "../../exercises.json";
 import { Link } from "react-router-dom";
-import Container from "../SelectionContainer/SelectionContainer"
+import Container from "../SelectionContainer/SelectionContainer";
+import "./style.css"
 import {
   BsFillInfoCircleFill
 } from "react-icons/bs";
@@ -18,31 +19,32 @@ const  Muscles = ()=>{
               <button key={muscles.nameID} type="button" onClick={() => {DisplayExercises(muscles.name)}} 
               className="btn btn-primary btn-lg btn-block">{muscles.name}</button>
           </div>
-          <DisplayExercises/>
         </div>  
       )
 
     }
   const DisplayExercises = (name) =>{
     console.log(name)
+    console.log(data)
     let muscle = data.filter(muscle => { return name === muscle.name})
     console.log(muscle)
-    muscle.map((muscle)=>{
+    muscle.map((exercises)=>{
+      console.log("digging " + muscle.exercises.exercise)
         return(
         <div className="mr-2 ml-2">
         <div className="col-md-4 mr-2 ml-2 mt-5 text-center justify-content-center">
-          <div className="card" style={{width: 18 + "rem"}}>
-              <img src={process.env.PUBLIC_URL + muscle.exercises.gif} className="card-img-top" alt="portfolio screenshot"/>
+          <div className="card" style={{width: 18 + "rem"}} key={exercises.exerciseID}>
+              <img src={process.env.PUBLIC_URL + exercises.gif} className="card-img-top" alt="portfolio screenshot"/>
                   <div className="card-head">
-                    <h5>{muscle.exercises.exercise}</h5>
-                  <button type="button" className="btn btn-custom show-btn " data-rel={muscle.exercises.exerciseID}>
+                    <h5>{exercises.exercise}</h5>
+                  <button type="button" className="btn btn-custom show-btn " data-rel={exercises.exerciseID}>
                       <BsFillInfoCircleFill/>
                   </button>
                   </div>
-                  <div className="card-reveal" data-rel={muscle.exercises.exercise} style={{display: "none"}}>
-                  <button type="button" className="close" data-rel={muscle.exercises.exercise} data-dismiss="modal" aria-label="Close">
+                  <div className="card-reveal" data-rel={exercises.exercise} style={{display: "none"}}>
+                  <button type="button" className="close" data-rel={exercises.exercise} data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span></button>
-                  <p>{muscle.exercises.description}</p>
+                  <p>{exercises.description}</p>
                 </div>
           </div> 
         </div> 
@@ -50,7 +52,6 @@ const  Muscles = ()=>{
         )
       })
     }
-  // }
   function changeImg() {
     return data.filter()
       document.getElementById("muscleImage").src = process.env.PUBLIC_URL + "muscles.image"
@@ -64,19 +65,8 @@ const  Muscles = ()=>{
       <div className="row justify-content-center">
         <img id="muscleImage" src="logo.svg" alt="muscle groups"></img>
       </div>
-        <div className="row justify-content-center">
-       
-     <Form/>
-
-        {/* {data.map(muscles=>(
-          <Form
-            key = {muscles.nameID}
-            image = {muscles.image}
-            id = {muscles.nameID}
-            name= {muscles.name}
-          />
-        ))} */}
-
+      <div className="row justify-content-center">
+        <Form/>
       </div>
       <div className="row justify-content-center">
         <li className="nav-item nav-link">
