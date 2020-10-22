50e118a2cb8fc2bbb5151e95e2fca9f79849eca3
Corvus <wyatta.hancock@gmail.com>
2020-10-22 10:29:58 -0600

    images and exercise cards are rendering

--- client/src/components/Musclecard/Exercisecard.js
+++ client/src/components/Musclecard/Exercisecard.js
@@ -21,21 +21,23 @@ export default function Card(props) {
         $('div.card-reveal[data-rel=' + $(this).data('rel') + ']').slideToggle('slow');
       });
     return(
-        <div className="col-md-4 mr-2 ml-2 mt-5 text-center justify-content-center">
-        <div className="card" style={{width: 18 + "rem"}} key={props.exerciseID}>
-            <img src={process.env.PUBLIC_URL + props.gif} className="card-img-top" alt="gif of exercise"/>
-                <div className="card-head">
-                  <h5>{props.exercise}</h5>
-                <button type="button" className="btn btn-custom show-btn " data-rel={props.exerciseID}>
-                    <BsFillInfoCircleFill/>
-                </button>
-                </div>
-                <div className="card-reveal" data-rel={props.exercise} style={{display: "none"}}>
-                <button type="button" className="close" data-rel={props.exercise} data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">×</span></button>
-                <p>{props.description}</p>
-              </div>
-        </div> 
-      </div> 
+        <div className="mr-2 ml-2">
+            <div className="col-md-4 mr-2 ml-2 mt-5 text-center justify-content-center">
+                <div className="card" style={{width: 18 + "rem"}} key={props.exerciseID}>
+                    <img src={process.env.PUBLIC_URL + props.gif} className="card-img-top" alt="gif of exercise"/>
+                        <div className="card-head">
+                        <h5>{props.exercise}</h5>
+                        <button type="button" className="btn btn-custom show-btn " data-rel={props.exerciseID}>
+                            <BsFillInfoCircleFill/>
+                        </button>
+                        </div>
+                        <div className="card-reveal" data-rel={props.exercise} style={{display: "none"}}>
+                        <button type="button" className="close" data-rel={props.exercise} data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span></button>
+                        <p>{props.description}</p>
+                    </div>
+                </div> 
+            </div> 
+        </div>
     )
 }
