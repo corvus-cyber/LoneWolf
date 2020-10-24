c9887ef2f9c5d2c6714c6a1ec22854d614426b8f
Dilan Li <dilanli0706@gmail.com>
2020-10-24 10:03:29 -0600

    started setting up for auth0

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.Fragment>
     <App />
-  </React.Fragment>,
+  </React.Fragment>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
