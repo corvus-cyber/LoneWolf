65bab49c1ec255e8bf13bc28b18fd2f0acb78538
Dilan Li <dilanli0706@gmail.com>
2020-10-24 11:56:51 -0600

    basic login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-
-import { HashRouter, Route} from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Callback from './Callback';
+import Home from './Home';
 import Exercises from "./pages/Chooseexercises"
 import Muscles from "./pages/Choosemuscles"
 import Workouts from "./pages/Createworkout"
 import Dashboard from "./pages/Datadashboard"
 import Stats from "./pages/Enterstats"
-import Login from "./pages/Login"
+// import Login from "./pages/Login"
+
 function App() {
-  
+
   return (
-    <HashRouter basename='/'>
-    <div>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard}/>
-          <Route exact path="/stats" component={Stats}/>
-          <Route exact path="/build" component={Muscles}/>
-          <Route exact path="/exercises" component={Exercises}/>
-          <Route exact path="/workouts" component={Workouts}/>
-    </div>
-  </HashRouter>
+    <Router basename='/'>
+      <div>
+        <Route exact path='/' component={Home} exact />
+        <Route exact path='/callback' component={Callback} exact />
+        {/* <Route exact path="/" component={Login} /> */}
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/stats" component={Stats} />
+        <Route exact path="/build" component={Muscles} />
+        <Route exact path="/exercises" component={Exercises} />
+        <Route exact path="/workouts" component={Workouts} />
+      </div>
+    </Router>
 
   );
 }
