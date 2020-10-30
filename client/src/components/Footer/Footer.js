6c0000a8691947b2d090873c3618078f7089f9f5
Corvus <wyatta.hancock@gmail.com>
2020-10-29 20:35:28 -0600

    improved login page

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,8 @@ function Footer(){
         <div className="row md-4 fixed-bottom">
         <div className="col-md-12 text-muted">
                 <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                <a className="copyrightLink" href="https://www.linkedin.com/in/wyatt-a-hancock-885705166/"> Wyatt A. Hancock</a>
+                <a className="copyrightLink" href="https://corvus-cyber.github.io/React_Portfolio/#/"> Wyatt Hancock and</a>
+                <a className="copyrightLink" href="https://dilanli.github.io/Profile-React/"> Dilan Li</a>
                 </div>
         </div> 
         </div>
