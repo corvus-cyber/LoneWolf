8333aec7fd8fca37c8be327cb17c6ad4474aea9f
Corvus <wyatta.hancock@gmail.com>
2020-11-02 10:08:51 -0700

    home is now up to spec

--- client/src/Home.js
+++ client/src/Home.js
@@ -32,7 +32,7 @@ function Home(props) {
     return (
         <div className="App" >
         <Navbar className="NavbarColor" dark expand="md">
-            <img className="Homelogo" src={process.env.PUBLIC_URL + "/White-logo.png"}></img>
+            <img className="Homelogo" src={process.env.PUBLIC_URL + "/assets/logo/lonewolflogo.png"}></img>
             <NavbarToggler onClick={toggle} />
             <Collapse isOpen={isOpen} navbar>
             <Nav className="ml-auto" navbar>
@@ -60,8 +60,35 @@ function Home(props) {
             </Nav>
             </Collapse>
         </Navbar>
-        <Hero></Hero>    
-            
+        <Hero/>
+        <Wrapper>
+        <div className="row">
+            <div className="col-md-8">
+                <img src={process.env.PUBLIC_URL + "/assets/images/Musclecircle.png"} className="muscleCircle" />
+            </div>
+            {/* <div className="row"> */}
+                <div className="col-md-4 mb-5 align-self-end">
+                    <h3 className="white">Choose How You Build</h3>
+                    <h6 className="white">Pick which muscle group you want to work on, select an exercise and then get to work!</h6>
+                </div>
+            {/* </div> */}
+        </div>    
+        <div className="row">
+            <div className="col-md-4 mt-5">
+                    <h3 className="white">So Drop The Weights</h3>
+                    <h6 className="white">And Hit The Floor!</h6>
+            </div>
+            <div className="col-md-8 align-self-end justify-content-end">
+                <img src={process.env.PUBLIC_URL + "/assets/images/droptheweights.png"} className="hitFloor justify-content-end float-right" />
+
+            </div>
+        </div>
+        <div className="row mt-5 pt-5">
+            <div className="col-md-12 text-center">
+                <h1 className="white">PUSH YOUR LIMITS</h1>
+            </div>
+        </div>
+        </Wrapper>    
         </div>
     )
 
