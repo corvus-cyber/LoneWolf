d7e3410c1c2528926df0463a0f03fbae8c8ab9c4
Corvus <wyatta.hancock@gmail.com>
2020-11-03 19:25:14 -0700

    pushing

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lonewolf",
 {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreatIndex: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
