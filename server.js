a334bb49b84163fedbb6ad4c1d10a5ed1f480e92
Dilan Li <dilanli0706@gmail.com>
2020-11-04 17:17:17 -0700

    updates on heroku routing issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use("/api", apiRoutes);
+app.use(apiRoutes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lonewolf",
