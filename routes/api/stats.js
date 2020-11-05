1c2feb49eb2f5930e994eaf4a9b00edfeb160f8d
Dilan Li <dilanli0706@gmail.com>
2020-11-04 17:16:46 -0700

    updates on heroku routing issue

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 const statsController = require("../../controllers/statsController");
 const repsAndTimeController = require("../../controllers/repsAndTimeController");
 
+router.use("/api");
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 // Matches with "/api/stats"
 router.route("/stats")
   .post(statsController.create)
@@ -11,5 +18,3 @@ router.route("/stats")
 router.route("/repsandtime")
   .post(repsAndTimeController.create)
   .get(repsAndTimeController.getRepsAndTimeStats)
-
-module.exports = router;
